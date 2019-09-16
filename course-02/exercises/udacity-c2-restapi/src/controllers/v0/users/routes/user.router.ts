033d79db67436b7fd5d4789f2be99dcf078d80f4
Brandon Soto <brandon.soto09@gmail.com>
2019-09-16 11:10:47 -0700

    First commit

---
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/user.router.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response } from 'express';
+
+import { User } from '../models/User';
+import { AuthRouter, requireAuth } from './auth.router';
+
+const router: Router = Router();
+
+router.use('/auth', AuthRouter);
+
+router.get('/', async (req: Request, res: Response) => {
+    // TODO: this is only for development
+    const items = await User.findAll();
+    res.send(items);
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await User.findByPk(id);
+    res.send(item);
+});
+
+export const UserRouter: Router = router;
